a9bcdc4dbd3738341a0b0057dd8f53fb5c927736
albertomola <pacmonkeykong@gmail.com>
2024-04-04 06:30:34 -0700

    Initial Commit

---
+++ Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by alberto on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
